821b2ff25d141cc741eac1cd98221776f281051f
Dian <dianshah95@gmail.com>
2017-09-28 17:43:33 +0800

    new UI

---
+++ UI/Home/home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!-- Template by quackit.com -->
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Homepage</title>
+	<link rel="stylesheet" 
+		  type="text/css"
+		  href="home.css"/>
+		
+</head>
+
+<body>
+
+	<header id="header"><p>Header...</p></header>
+
+	<div class="container">
+
+		<div class="row">
+		
+			<div class="col-sm-4 text-center">
+				<img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt>
+				<h3>"Search"</h3>
+				<p> ahuhsihsswsisjwijs </p>
+			</div>
+			
+			<div class="col-sm-4 text-center">
+				<img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt>
+				<h3>"Search"</h3>
+				<p> ahuhsihsswsisjwijs </p>
+			</div>
+			
+		</div>
+	</div>
+
+	<div id="footer-wrapper">
+		<footer id="footer"><p>Footer...</p></footer>
+	</div>
+
+</body>
+
+</html>
