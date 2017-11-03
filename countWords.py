3a1c34cd57abc00b32bce431b9973adaa7070026
msfrhdmla <musfirahdamla@gmail.com>
2017-11-03 15:06:36 +0800

    Add files via upload

--- countWords.py
+++ countWords.py
@@ -1,16 +1,33 @@
 from __future__ import division
 
-wordInsentence = []
 
-### Sentence is a string e.g (hari ini hari sabtu
-### key is keyword (hari)
+# Sentence HAS to be a LIST
+wordsDictionaryFreq = {}
+words = []
 
-def count(sentence, key):
+list = {
+    "kd puuru busuk", "raam busuk", "messi busuk sangat", "dian comel", "hari ini hari busuk", "hari hari hari jumaat"
+}
+def freqDictionary(list):
 
-    total = 0
-    for a in sentence.split():
-        if a == key:
-            total = total + 1
-    totalWords = len(sentence.split())
-    percentage = (float(total / totalWords)) * 100
-    return percentage
+    for line in list:
+        for w in line.split():
+            words.append(w.lower())
+
+
+    for w in words:
+        count = 0
+        for w2 in words:
+            if w2 == w:
+                count = count + 1
+        wordsDictionaryFreq[w] = count
+
+    return wordsDictionaryFreq
+
+# Kasi List and keywords
+def getPercentage(key,list):
+    freqDictionary(list)
+    totalkey = wordsDictionaryFreq.get(key)
+    totalw = sum(wordsDictionaryFreq.values())
+    percent = (totalkey/totalw)*100
+    return percent
