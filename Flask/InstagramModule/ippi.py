5b06d7fcbfd0613c3d4fb72a5c022cf3b0c26da4
Nyanaraam <raam3395@gmail.com>
2017-11-14 18:29:02 +0800

    Add files via upload

--- Flask/InstagramModule/ippi.py
+++ Flask/InstagramModule/ippi.py
@@ -3,8 +3,9 @@ import json
 import time
 from datetime import date,datetime
 from dateutil import tz
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from instagram.client import InstagramAPI
+from key import GoogleKey, InstaClient_id, InstaClient_secret, InstaToken
 import pytz
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
@@ -14,10 +15,13 @@ from instagram.bind import InstagramAPIError
 app = Flask(__name__)
 
 class Instagram():
-    token = "38254173.b20129c.6db34c4c1f80445fa9d3946e84fdd50d" # Obtained from https://www.instagram.com/developer/
-    client_id = "b20129c9e6c04c009d310c99b0d490e0" # Obtained from https://www.instagram.com/developer/
-    client_secret = "e0fc4d2c42734a668096137bb4cd30ed" # Obtained from https://www.instagram.com/developer/
-    api = InstagramAPI(access_token=token, client_secret=client_secret)
+    profile = ""
+    client_id = InstaClient_id
+    api = InstagramAPI(access_token=InstaToken, client_secret=InstaClient_secret)
+
+    app.config['GOOGLEMAPS_KEY'] = GoogleKey
+    GoogleMaps(app)
+
 
     def user_search(self,nama):
         profile = self.api.user_search(q = nama)
@@ -32,20 +36,20 @@ class Instagram():
         return profile
 
     def user_media(self, userID):
-        recent_media, nextt = self.api.user_recent_media(user_id=userID, count=10)
+        recent_media , nextt = self.api.user_recent_media(user_id=userID, count=20)
+        medialist = []
         if not recent_media:
             print "Can't retrieve user media"
-        else:
+        """else:
             for media in recent_media:
-
                 print "Id = " + media.id
                 print "Image = " + media.images['standard_resolution'].url
                 print "Caption = " + media.caption.text
                 print "Time = " , media.created_time.astimezone(pytz.timezone('Asia/Kuala_Lumpur'))
-                """print "Likes = " , media.likes.count"""
-                print "Location = " + media.location.name
+                #print "Likes = " , media.likes.count
+                #print "Location = " + media.location.name"""
 
-                print
+        return recent_media
 
     def comment_media(self, mediaID):
         recent_comment = self.api.media_comments(media_id=mediaID)
@@ -62,6 +66,7 @@ class Instagram():
         #    print "Profile is private"
         #else:
         print "Id = " + profile.id
+        print profile
         print "Username : " + profile.username
         print "Profile Picture : " + profile.profile_picture
         print "Name : " + profile.full_name
@@ -73,8 +78,11 @@ class Instagram():
         print "Follows : ", profile.counts['follows']
         print "Follow by : ", profile.counts['followed_by']
 
+        return profile
+
     def location(self, userID):
         recent_media, nextt = self.api.user_recent_media(user_id=userID, count=10)
+
         if not recent_media:
             print "Can't retrieve user media"
         else:
@@ -86,16 +94,16 @@ class Instagram():
 
 hai = Instagram()
 
-'''var = raw_input("Who do you want to search?")
+"""var = raw_input("Who do you want to search?")
 hai.user_search(var)
 var1 = raw_input("Which profile do you want to view?")
 hai.user_profile(var1)
 print
-#hai.user_media(var1)
+hai.user_media(var1)
 var2 = raw_input("Which image do you want to view the comments?")
-hai.comment_media(var2)
+hai.comment_media(var2)"""
 #var3 = raw_input("User previous location")
-#hai.location(var3)'''
+#hai.location(var3)
 
 
 
@@ -110,13 +118,19 @@ def user_search(word):
     profile = hai.user_search(word)
     return render_template("search.html", result=profile)
 
-@app.route("/profile/<username>")
-def profile(username):
-    user = hai.user_profile(username)
-    print user
-    #media = hai.user_media(username)
+@app.route("/profile/<id>")
+def user_profile(id):
 
-    return render_template("Instagram.html", user = user)
+    profile = hai.user_profile(id)
+    print "here"
+    print "Username : " + profile.username
+    print profile.full_name
+
+    media = hai.user_media(id)
+    print media
+
+
+    return render_template("Instagram.html", user = profile, media = media)
 app.run(debug=True)
 
 #            <!--<a href="{{ url_for('profile', username = result.id) }}"> View profile :</a>-->
