096536725fd938b4196f347c4deae8568ec1f6e3
Nyanaraam <raam3395@gmail.com>
2017-11-23 23:56:53 +0800

    Updated Backend

--- Flask/InstagramModule/key.py
+++ Flask/InstagramModule/key.py
@@ -1,4 +1,5 @@
-GoogleKey="AIzaSyCQaXdeh30YGYlYPK6eqt9AcAJC4or5I8w"
-InstaToken = "38254173.b20129c.6db34c4c1f80445fa9d3946e84fdd50d" # Obtained from https://www.instagram.com/developer/
-InstaClient_id = "b20129c9e6c04c009d310c99b0d490e0" # Obtained from https://www.instagram.com/developer/
-InstaClient_secret = "e0fc4d2c42734a668096137bb4cd30ed" # Obtained from https://www.instagram.com/developer/
+GOOGLEKEY="AIzaSyCQaXdeh30YGYlYPK6eqt9AcAJC4or5I8w"
+INSTATOKEN = "38254173.b20129c.6db34c4c1f80445fa9d3946e84fdd50d" # Obtained from https://www.instagram.com/developer/
+INSTACLIENT_ID = "b20129c9e6c04c009d310c99b0d490e0" # Obtained from https://www.instagram.com/developer/
+INSTACLIENT_SECRET = "e0fc4d2c42734a668096137bb4cd30ed" # Obtained from https://www.instagram.com/developer/
+SECRET_KEY = "heheheparlo"
