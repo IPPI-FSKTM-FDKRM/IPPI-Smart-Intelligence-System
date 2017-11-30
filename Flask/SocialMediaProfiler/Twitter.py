4c7d00bf3fbc875af6dabaa74299577aee627572
firdaus sahran <firdaussahran@gmail.com>
2017-11-30 20:31:42 +0800

    twitter integration messi

---
+++ Flask/SocialMediaProfiler/Twitter.py
@@ -0,0 +1,192 @@
+
+
+from twitter import *
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request, jsonify, url_for
+from TwitterSearch import *
+from collections import *
+
+
+app = Flask(__name__)
+broker_url = 'amqp://guest@localhost'
+
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+config = {}
+execfile("config.py", config)
+
+class Twitter():
+
+
+    twitter = Twitter(
+		auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
+
+    #username = raw_input("Enter username to search: @")
+
+    def searchUser(self,username):
+        user = self.twitter.users.search(q = username)
+        return user
+
+    def getUserProfile(self,username):
+        user = self.twitter.users.search(q = username)
+        for user in user:
+            print "Profile image: %s" % user["profile_image_url"]
+            print "Username: @%s" % user["screen_name"]
+            print "Name: %s" % user["name"]
+            print "Twitter ID: %s" % user["id"]
+            print "Bio: %s" % user["description"]
+            print "Followers: %s " % user["followers_count"]
+            print "Account created since: %s" %  user["created_at"]
+        return user
+
+    followingName = []
+    followingImgURL = []
+    followingCount = 0
+
+    def getFollowingCount(self):
+        return self.followingCount
+
+    def getFollowingName(self):
+        return self.followingName
+
+    def getFollowingImgURL(self):
+        return self.followingImgURL
+
+    def getFollowingInfo(self,username):
+        friends = self.twitter.friends.ids(screen_name=username)
+        self.followingCount = len(friends["ids"])
+        for n in range(0, len(friends["ids"]), 100):
+            ids = friends["ids"][n:n+100]
+
+            subquery = self.twitter.users.lookup(user_id = ids)
+            for user in subquery:
+                #print " [%s] %s - %s [%s]" % ("*" if user["verified"] else " ", user["screen_name"], user["location"], user["description"])
+                name = ''
+                name = name + user["screen_name"].encode('utf-8')
+                self.followingName.append(name)
+                url = ''
+                url = url + user["profile_image_url"].encode('utf-8')
+                self.followingImgURL.append(url)
+
+    tweet = []
+    tweetSource = []
+    tweetTime = []
+    tweetLocation = []
+
+    def getTweetList(self):
+        return self.tweet
+
+    def getTweetSource(self):
+        return self.tweetSource
+
+    def getTweetTime(self):
+        return self.tweetTime
+
+    def getTweetLocation(self):
+        return self.tweetLocation
+
+    def getTweets(self, username):
+        tw = self.twitter.statuses.user_timeline(screen_name=username, exclude_replies="false")
+        # print tweet
+        for tw in tw:
+            t = ''
+            t = t + tw["text"]
+            self.tweet.append(t)
+            source = ''
+            source = source + tw["source"].encode('utf-8')
+            self.tweetSource.append(source)
+            time = ''
+            time = time + tw["created_at"].encode('utf-8')
+            self.tweetTime.append(time)
+            coordinates = tw["coordinates"]
+            str = {}
+            if coordinates != None:
+                str = tw["coordinates"]
+            else:
+                continue
+            self.tweetLocation.append(str)
+
+    followerName = []
+    followerImgURL = []
+    followerCount = 0
+
+    def getFollowerName(self):
+        return self.followerName
+
+    def getFollowerImgURL(self):
+        return self.followerImgURL
+
+    def getFollowerCount(self):
+        return self.followerCount
+
+    def getFollowerInfo(self,username):
+        count = self.twitter.followers.ids(screen_name=username)
+        self.followerCount = len(count["ids"])
+        for n in range(0, len(count["ids"]), 100):
+            ids = count["ids"][n:n + 100]
+            #
+            subquery = self.twitter.users.lookup(user_id=ids)
+            for user in subquery:
+                # print " [%s] %s - %s [%s]" % ("*" if user["verified"] else " ", user["screen_name"], user["location"], user["description"])
+                name = ''
+                name = name + user["screen_name"].encode('utf-8')
+                self.followerName.append(name)
+                url = ''
+                url = url + user["profile_image_url"].encode('utf-8')
+                self.followerImgURL.append(url)
+
+x = Twitter()
+
+@app.route("/")
+def index():
+    return "WELCOME"
+
+@app.route("/Twitter/search")
+def search():
+
+    return render_template("Twitter_search.html")
+
+@app.route("/Twitter/searchResult",  methods=['POST'])
+def twitterSearchResult():
+    data = json.loads(request.data)
+    result  = x.searchUser(data)
+    print result
+
+    return jsonify({'result': render_template("searchResultTwitter.html", result=result)})
+
+@app.route("/profile/<username>")
+def twitterProfile(username):
+    user = x.getUserProfile(username)
+    x.getFollowingInfo(username)
+    followingCount = x.getFollowingCount()
+    followingName = x.getFollowingName()
+    followingImgURL = x.getFollowingImgURL()
+
+    x.getFollowerInfo(username)
+    followerCount = x.getFollowerCount()
+    followerName = x.getFollowerName()
+    followerImgURL = x.getFollowerImgURL()
+
+    x.getTweets(username)
+    getTweetList = x.getTweetList()
+    getTweetSource = x.getTweetSource()
+    getTweetTime = x.getTweetTime()
+    getTweetLocation = x.getTweetLocation()
+
+    return render_template("Twitter.html", user=user, followingCount=followingCount, followingName=followingName, followingImgURL=followingImgURL,
+                           followerCount=followerCount, followerName=followerName, followerImgURL=followerImgURL,
+                           getTweetList=getTweetList, getTweetSource=getTweetSource, getTweetTime=getTweetTime, getTweetLocation=getTweetLocation)
+
+app.static_folder = 'static'
+app.run(debug=True)
+
+#x = Twitter()
+#x.getUserProfile()
+#x.getConnections()
+#x.getTweets()
+#x.getFriendship()
+
---
+++ Flask/SocialMediaProfiler/config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "jbF41tG34JzNQvrvBa5Ed1jmQ"
+consumer_secret = "1Rxi38DBRkzlKzTymNTnWf4QXW6vooe4hiaYS3cwtJIjgRTv88"
+access_key = "1322155442-48vSSWMCw6xyKXb2UAPbmSp14Lm34IL6NJB9AQl"
+access_secret = "D0IU0LMXMCR1lmyBNrekpZ1bIT4RK68DQ7Y7svGkpjjb5"
