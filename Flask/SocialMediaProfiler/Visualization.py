40f1a2ae94b9c92bd9ef9f9aa515cc35b68e3e90
msfrhdmla <musfirahdamla@gmail.com>
2017-12-07 15:18:51 +0800

    Mus - parameter color added to visualization

--- Flask/SocialMediaProfiler/Visualization.py
+++ Flask/SocialMediaProfiler/Visualization.py
@@ -1,35 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def pieChart(dict):
+def pieChart(dict,color):
     yv = dict.values()
     x = dict.keys()
     y = []
-    plt.figure()
     for a in yv:
         y.append(len(a))
     fig, ax = plt.subplots()
     ax.axis('equal')
-    patches,text, autotext = ax.pie(y, radius=1.3, labels=x, colors=np.random.rand(7,4),autopct='%d%%')
+    patches,text, autotext = ax.pie(y, radius=1.3, labels=x, colors=np.random.rand(5,3),autopct='%d%%')
     for i in range(0,len(text)):
-        text[i].set_color('white')
+        text[i].set_color(color)
     return plt
 
-def pieChartSentiment(dict):
+def pieChartSentiment(dict,color):
     yv = dict.values()
     x = dict.keys()
     y = []
     for a in yv:
         y.append(len(a))
-    plt.figure()
     fig, ax = plt.subplots()
     ax.axis('equal')
     patches,text, autotext = ax.pie(y, radius=1.3, labels=x, colors=['lightskyblue','gold','lightcoral'],autopct='%d%%')
     for i in range(0,len(text)):
-        text[i].set_color('white')
+        text[i].set_color(color)
     return plt
 
-def lineChartTime(dict,stringX,stringY):
+def lineChartTime(dict,stringX,stringY,color):
     newdict = {}
     for key in dict.keys():
         nkey = int(key.encode('utf=8'))
@@ -42,17 +40,17 @@ def lineChartTime(dict,stringX,stringY):
     plt.figure()
     plt.plot(x,y,'w-')
     plt.xticks(x)
-    plt.xlabel(stringX,{'color':'white'})
-    plt.ylabel(stringY,{'color':'white'})
+    plt.xlabel(stringX,{'color':color})
+    plt.ylabel(stringY,{'color':color})
     ax = plt.subplot()
     for i in ax.spines:
-        ax.spines[i].set_color('white')
-    ax.xaxis.label.set_color('white')
-    ax.yaxis.label.set_color('white')
-    ax.tick_params(axis='x', colors='white')
-    ax.tick_params(axis='y', colors='white')
+        ax.spines[i].set_color(color)
+    ax.xaxis.label.set_color(color)
+    ax.yaxis.label.set_color(color)
+    ax.tick_params(axis='x', colors=color)
+    ax.tick_params(axis='y', colors=color)
     return plt
-def lineChart(dict,stringX,stringY):
+def lineChart(dict,stringX,stringY,color):
     x = dict.keys()
     yv = dict.values()
     y =[]
@@ -61,18 +59,18 @@ def lineChart(dict,stringX,stringY):
     plt.figure()
     plt.plot(x,y,'w-')
     plt.xticks(x)
-    plt.xlabel(stringX,{'color':'white'})
-    plt.ylabel(stringY,{'color':'white'})
+    plt.xlabel(stringX,{'color':color})
+    plt.ylabel(stringY,{'color':color})
     ax = plt.subplot()
     for i in ax.spines:
-        ax.spines[i].set_color('white')
-    ax.xaxis.label.set_color('white')
-    ax.yaxis.label.set_color('white')
-    ax.tick_params(axis='x', colors='white')
-    ax.tick_params(axis='y', colors='white')
+        ax.spines[i].set_color(color)
+    ax.xaxis.label.set_color(color)
+    ax.yaxis.label.set_color(color)
+    ax.tick_params(axis='x', colors=color)
+    ax.tick_params(axis='y', colors=color)
     return plt
 
-def barChartTime(dict,stringX,stringY):
+def barChartTime(dict,stringX,stringY,color):
     newdict = {}
     for key in dict.keys():
         nkey = int(key.encode('utf=8'))
@@ -87,21 +85,21 @@ def barChartTime(dict,stringX,stringY):
     plt.ylim((min(y)+0.5),(max(y)+0.5))
     bar = plt.bar(y_plot, y, align='center')
     for i in range(0,len(bar)):
-       bar[i].set_color("white")
+       bar[i].set_color(color)
     plt.xticks(y_plot, x)
     ax = plt.subplot()
     ax.set_ylim(ymin=0)
-    plt.ylabel(stringY,{'color':'white'})
-    plt.xlabel(stringX,{'color':'white'})
+    plt.ylabel(stringY,{'color':color})
+    plt.xlabel(stringX,{'color':color})
     for i in ax.spines:
-        ax.spines[i].set_color('white')
-    ax.xaxis.label.set_color('white')
-    ax.yaxis.label.set_color('white')
-    ax.tick_params(axis='x', colors='white')
-    ax.tick_params(axis='y', colors='white')
+        ax.spines[i].set_color(color)
+    ax.xaxis.label.set_color(color)
+    ax.yaxis.label.set_color(color)
+    ax.tick_params(axis='x', colors=color)
+    ax.tick_params(axis='y', colors=color)
     return plt
 
-def barChart(dict,stringX,stringY):
+def barChart(dict,stringX,stringY,color):
     plt.figure()
     x = dict.keys()
     y_plot = np.arange(len(x))
@@ -112,15 +110,15 @@ def barChart(dict,stringX,stringY):
     plt.ylim((min(y)+0.5),(max(y)+0.5))
     bar = plt.bar(y_plot, y, align='center')
     for i in range(0,len(bar)):
-       bar[i].set_color("white")
+       bar[i].set_color(color)
     plt.xticks(y_plot, x)
     ax = plt.subplot()
-    plt.ylabel(stringY,{'color':'white'})
-    plt.xlabel(stringX,{'color':'white'})
+    plt.ylabel(stringY,{'color':color})
+    plt.xlabel(stringX,{'color':color})
     for i in ax.spines:
-        ax.spines[i].set_color('white')
-    ax.xaxis.label.set_color('white')
-    ax.yaxis.label.set_color('white')
-    ax.tick_params(axis='x', colors='white')
-    ax.tick_params(axis='y', colors='white')
+        ax.spines[i].set_color(color)
+    ax.xaxis.label.set_color(color)
+    ax.yaxis.label.set_color(color)
+    ax.tick_params(axis='x', colors=color)
+    ax.tick_params(axis='y', colors=color)
     return plt
