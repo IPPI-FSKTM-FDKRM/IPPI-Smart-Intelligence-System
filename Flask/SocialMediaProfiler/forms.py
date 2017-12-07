0cd0661f658cb9eb9673b21e498df00f2c00b833
Nyanaraam <raam3395@gmail.com>
2017-12-08 02:00:04 +0800

    something that raam did

--- Flask/SocialMediaProfiler/forms.py
+++ Flask/SocialMediaProfiler/forms.py
@@ -7,10 +7,15 @@ class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    #first_name = StringField('first name', validators=[InputRequired()])
-    #last_name = StringField('last name', validators=[InputRequired()])
+    first_name = StringField('first name', validators=[InputRequired(), Length(min=1, max=50)])
+    last_name = StringField('last name', validators=[InputRequired(),Length(min=1, max=50)])
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
+
+class ChangePassword(FlaskForm):
+    password1 = PasswordField('password1', validators=[InputRequired(), Length(min=8, max=80)])
+    #password2 = PasswordField('password2', validators=[InputRequired(), Length(min=8, max=80)])
+
